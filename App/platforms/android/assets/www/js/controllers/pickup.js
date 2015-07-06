041c47aefa90980af4c26a08871ff2f0516395da
alexkates <daniel.alexander.kates@gmail.com>
2015-07-06 19:10:12 -0400

    fixed an issue that caused chrome dev errors when scrolling the map

--- App/platforms/android/assets/www/js/controllers/pickup.js
+++ App/platforms/android/assets/www/js/controllers/pickup.js
@@ -1,10 +1,17 @@
 angular.module("controllers").controller("PickupController", function ($scope, LocationService) {
     LocationService.getLocation().then(function (position) {
         $scope.map = {
-            center: position.coords,
+            center:
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            },
             zoom: 12
         };
 
-        $scope.userLocation = position.coords;
+        $scope.userLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
     });
 });
